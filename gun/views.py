45eb037196494e7867ff98fbd668e8067050bc9d
Techbite <bonfacemwema7@gmail.com>
2024-01-26 10:57:32 +0300

    first commit

---
+++ gun/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
+
+
+def send_welcome_email(name, email):
+    # Customize the email content as per your requirements
+    subject = "Welcome to Our Website"
+    message = (
+        f"Hello, {name}, Welcome to our website! Check your email for more information"
+    )
+    from_email = "bonfacemwema7@gmail.com"  # Update with your email
+    recipient_list = [email]
+
+    try:
+        send_mail(subject, message, from_email, recipient_list)
+        return True
+    except Exception as e:
+        return False
+
+
+def index(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+
+        if send_welcome_email(name, email):
+            messages.success(request, "A welcome email has successfully been sent to "+email+". Check your email for more information")
+        else:
+            messages.error(request, "Error sending email. Please try again.")
+
+    return render(request, "index.html")
