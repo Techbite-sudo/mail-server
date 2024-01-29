ba8c128c21540436592b7505be70c072433de3f3
Techbite <bonfacemwema7@gmail.com>
2024-01-29 20:45:45 +0300

    New message description

--- gun/views.py
+++ gun/views.py
@@ -9,7 +9,7 @@ def send_welcome_email(name, email):
     # Customize the email content as per your requirements
     subject = "Welcome to Our Website"
     message = (
-        f"Hello, {name}, Welcome to our website! Check your email for more information"
+        f"Hello, {name}, Welcome to Rapunzel Hair Affair where beauty meets elegance! In accordance to our esteemed motto we offer hair dressing, nail decoration, piercing and tatooing services. Get to book one of the sessions to enjoy the services we render.Our achievement is to have a satisfied customer."
     )
     from_email = "bonfacemwema7@gmail.com"  # Update with your email
     recipient_list = [email]
