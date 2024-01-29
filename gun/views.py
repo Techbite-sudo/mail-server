f098d7636e012692671cf9d706005b72643fa447
Techbite <bonfacemwema7@gmail.com>
2024-01-30 00:30:24 +0300

    mail changes

--- gun/views.py
+++ gun/views.py
@@ -11,7 +11,7 @@ def send_welcome_email(name, email):
     message = (
         f"Hello, {name}, Welcome to Rapunzel Hair Affair where beauty meets elegance! In accordance to our esteemed motto we offer hair dressing, nail decoration, piercing and tatooing services. Get to book one of the sessions to enjoy the services we render.Our achievement is to have a satisfied customer."
     )
-    from_email = "bonfacemwema7@gmail.com"  # Update with your email
+    from_email = "mashphotographer38@gmail.com"  # Update with your email
     recipient_list = [email]
 
     try:
